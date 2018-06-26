4cb58b9e98548108e529d826eba32c38a925bf4d
Luis Chan <luis.chan@qq.com>
2018-06-26 17:05:03 +0800

    Initial import

---
+++ src/main/java/com/example/jx/jxspringbootdemo/JxSpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.jx.jxspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxSpringBootDemoApplication.class, args);
+    }
+}
